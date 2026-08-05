57250671b4ddd8cfb3ec32a611bafbeeba9f9aad
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-08-04 21:44:30 -0300

    exercicios aula 1 e aula 2

---
+++ aula1.php
@@ -0,0 +1,28 @@
+<?php
+
+$nome = "Ariel";
+$idade = 32;
+$cidade = "Bento Gonçalves";
+$curso = "PHP";
+
+echo "Ola, " . $nome . ". Sua idade é: " . $idade . ". A cidade é: " . $cidade . ". O curso é: " . $curso;
+// Ola, Ariel. Sua idade é: 32. A cidade é: Bento Gonçalves. O curso é: PHP
+
+
+/**
+ * TIPOS DE VALORES COM CASAS DECIMAIS
+ * FLOAT
+ * DOUBLE
+ * LONG
+ * LONG DOUBLE
+ * NUMERIC
+ * DECIMAL
+ * REAL
+ */
+
+/**
+ * BOOLEAN | BOl | BOOL
+ * FALSE || TRUE
+ * 0 || 1
+ * Desligado || Ligado
+ */
