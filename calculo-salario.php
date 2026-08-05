f00e3a28e47f1a13c52032ef50d342f264b5ee82
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-08-04 21:59:59 -0300

    exercicio pag 17

---
+++ calculo-salario.php
@@ -0,0 +1,11 @@
+<?php
+
+$salario = 2500;
+$bonus = 300;
+$desconto = 200;
+
+$salarioFinal = $salario + $bonus - $desconto;
+
+// echo "O Salário final é: " . $salarioFinal;
+// echo "O Salário final é: {$salarioFinal}";
+echo "O Salário final é: $salarioFinal";
